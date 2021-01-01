3a0da9a197ec3ba871f948d36b1ca807c198c6e6
ReshamSaharan11 <43739491+ReshamSaharan11@users.noreply.github.com>
2021-01-01 20:31:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
+    user : 'postgres',
+    password : '1234',
     database : 'smart-brain'
   }
 });
